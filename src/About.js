a9b3e39e9998b8d9f788e4dffff7fa7200b7312d
Hiru <angelyeung238@gmail.com>
2024-09-06 15:45:43 +0800

    update

--- src/About.js
+++ src/About.js
@@ -16,15 +16,13 @@ const About = () => {
             2002年3月8日 香港<br />
             Programmer & CG Illustrator
           </p>
+          <div className='P'>
+            8年電腦繪圖經驗 13歲開始使用Sai作畫<br />
+            中學時期參加各種創天同人展販賣作品<br />
+            現今活躍於接委託<br />
+            跟商界 個別人士合作建立Project<br />
+            例如遊戲設計 宣傳圖等
         </div>
-        <div className='P'>
-        8年電腦繪圖經驗 13歲開始使用Sai作畫<br />
-        中學時期參加各種創天同人展販賣作品<br />
-        現今活躍於接委託<br />
-        跟商界 個別人士合作建立Project<br />
-        例如遊戲設計 宣傳圖等
-        </div>
-
         <div className='Tale'>
           <p>
             使用軟件為 PhotoShop, Clip Studio Paint
@@ -35,6 +33,10 @@ const About = () => {
           </p>
           <img src={IMG_888} className='IMG_888'/>
         </div>
+        </div>
+      
+
+        
 
     </div>
   )
