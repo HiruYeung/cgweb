5de5ffe4757b91b29bf3f8a590fbd542068b366a
hiru <ehiruac@gmail.com>
2024-10-25 01:18:19 +0800

    update

--- src/Gamepage/page.js
+++ src/Gamepage/page.js
@@ -21,7 +21,7 @@ const Page = () => {
         <div className='Plays' style={{ display: 'flex', gap: '0',}}>
           <img src={Blue1} className="Blue1" style={{ display: 'flex', gap: '0', width:'27%'}}/>
           <img onClick={() => setGameOpen(true)} src={Play} className="Play"style={{ display: 'flex', gap: '0', width: '44.7%'}}/>
-          <img src={Blue2} className="Blue2" style={{ display: 'flex', gap: '0', width:'28.40%'}}/>
+          <img src={Blue2} className="Blue2" style={{ display: 'flex', gap: '0', width:'28.395%'}}/>
         </div>
         
         <Modal keepMounted open={gameOpen} onClose={() => setGameOpen(false)}>
