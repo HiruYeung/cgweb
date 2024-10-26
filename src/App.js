4a6f4a00fa3cd18bfab76fd8989da0ace5d7a63d
hiru <ehiruac@gmail.com>
2024-10-26 14:30:24 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Root from "./Root";
 import About from "./About";
 import Blog from "./Blog";
